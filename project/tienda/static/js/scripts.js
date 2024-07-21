3efe604a6470828197dd467f61412a3bf960224c
yeyderjhjl <jturpoan.@unsa.edu.pe>
2024-07-21 08:31:06 -0500

    correción de delete

--- project/tienda/static/js/scripts.js
+++ project/tienda/static/js/scripts.js
@@ -4,3 +4,9 @@ function confirmLogout(event) {
         window.location.href = event.target.href;
     }
 }
+function confirmDelete(event) {
+    event.preventDefault();
+    if (confirm("¿Estás seguro de que deseas eliminar tu cuenta?")) {
+        window.location.href = event.target.href;
+    }
+}
