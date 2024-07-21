eb811995cf06d44c3767a4aa8f65aab6c3b51095
Daniel Chura <dchuramo@unsa.edu.pe>
2024-07-21 10:29:17 -0500

    agregando navbar

--- project/tienda/urls.py
+++ project/tienda/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     # paths for categories
     path('calendar/', views.calendar_view, name='calendar'),
-        path('calendar/events/', views.calendar_events, name='calendar_events'),
+    path('calendar2/', views.calendar2, name='calendar2'),
+
+    path('calendar/events/', views.calendar_events, name='calendar_events'),
 
     path('obtener_eventos/', views.obtener_eventos, name='obtener_eventos'),
-    path('calendar2/', views.calendar2, name='calendar2'),
+    
 
 ]
