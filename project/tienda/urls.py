7a592f41c000fe83b28f1d394f15e56f9e689e13
Daniel Chura <dchuramo@unsa.edu.pe>
2024-07-20 22:42:51 -0500

    cambios calendarios

--- project/tienda/urls.py
+++ project/tienda/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     # paths for categories
     path('calendar/', views.calendar_view, name='calendar'),
+        path('calendar/events/', views.calendar_events, name='calendar_events'),
+
+    path('obtener_eventos/', views.obtener_eventos, name='obtener_eventos'),
+    path('calendar2/', views.calendar2, name='calendar2'),
 
 ]
--- project/tienda/views.py
+++ project/tienda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Evento
 
 def index(request):
     return render(request, 'index.html')
@@ -8,3 +9,29 @@ def index(request):
 def calendar_view(request):
     return render(request, 'calendar.html')
 
+
+def calendar2(request):
+    return render(request, 'calendar2.html')
+
+@csrf_exempt
+def calendar_events(request):
+    events = Evento.objects.all()
+    event_list = []
+    for event in events:
+        event_list.append({
+            'title': event.sercod.sernom,
+            'start': event.evefec.isoformat(),
+            'description': f"Cliente: {event.clidni}, Personal: {event.perdni}"
+        })
+    return JsonResponse(event_list, safe=False)
+
+def obtener_eventos(request):
+    eventos = Evento.objects.all().select_related('sercod', 'perdni')
+    eventos_json = [
+        {
+            'fecha': evento.evefec.strftime('%Y-%m-%d'),
+            'servicio': evento.sercod.sernom,
+            'tecnico': evento.perdni.nombre_completo  # Asumiendo que tienes un campo nombre_completo en el modelo Personal
+        }
+    for evento in eventos]
+    return JsonResponse(eventos_json, safe=False)
